e339b896e0cc479d361415bb4fb09286bd677ceb
Matthieu Bucchianeri <mbucchia@gmail.com>
2022-06-12 15:36:51 -0700

    Prepare release 0.1.7

--- companion/Properties/AssemblyInfo.cs
+++ companion/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("companion")]
-[assembly: AssemblyCopyright("Copyright ?? Matthieu Bucchianeri 2022")]
+[assembly: AssemblyCopyright("Copyright 2022 Matthieu Bucchianeri")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
