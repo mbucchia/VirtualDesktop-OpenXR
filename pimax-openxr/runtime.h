10589aaedfc91e090bb39730ed6fb88caddeb7c2
Matthieu Bucchianeri <mbucchia@gmail.com>
2022-06-19 18:11:24 -0700

    Prepare release 0.2.0.

--- pimax-openxr/runtime.h
+++ pimax-openxr/runtime.h
@@ -33,8 +33,8 @@ namespace pimax_openxr {
     using namespace pimax_openxr::utils;
 
     const unsigned int RuntimeVersionMajor = 0;
-    const unsigned int RuntimeVersionMinor = 1;
-    const unsigned int RuntimeVersionPatch = 7;
+    const unsigned int RuntimeVersionMinor = 2;
+    const unsigned int RuntimeVersionPatch = 0;
 
     const std::string RuntimeName = "pimax-openxr";
     const std::string RuntimePrettyName =
