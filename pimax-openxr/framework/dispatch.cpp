ce82f12692126fd998a085ee1a7e6cc8add056df
Matthieu Bucchianeri <mbucchia@gmail.com>
2022-07-30 14:50:35 -0700

    Use Start/Stop for API functions in traces (fix missing ones).

--- pimax-openxr/framework/dispatch.cpp
+++ pimax-openxr/framework/dispatch.cpp
@@ -37,18 +37,22 @@ namespace RUNTIME_NAMESPACE {
 
     // Handle cleanup of the layer's singleton.
     XrResult XRAPI_CALL xrDestroyInstance(XrInstance instance) {
+        TraceLocalActivity(local);
+        TraceLoggingWriteStart(local, "xrDestroyInstance");
+
         XrResult result;
         try {
             result = RUNTIME_NAMESPACE::GetInstance()->xrDestroyInstance(instance);
             if (XR_SUCCEEDED(result)) {
                 RUNTIME_NAMESPACE::ResetInstance();
             }
-        } catch (std::runtime_error& exc) {
+        } catch (std::exception& exc) {
+            TraceLoggingWriteTagged(local, "xrDestroyInstance_Error", TLArg(exc.what(), "Error"));
             ErrorLog("xrDestroyInstance: %s\n", exc.what());
             result = XR_ERROR_RUNTIME_FAILURE;
         }
 
-        TraceLoggingWrite(g_traceProvider, "xrDestroyInstance_Result", TLArg(xr::ToCString(result), "Result"));
+        TraceLoggingWriteStop(local, "xrDestroyInstance", TLArg(xr::ToCString(result), "Result"));
         if (XR_FAILED(result)) {
             ErrorLog("xrDestroyInstance failed with %s\n", xr::ToCString(result));
         }
@@ -58,12 +62,24 @@ namespace RUNTIME_NAMESPACE {
 
     // Forward the xrGetInstanceProcAddr() call to the dispatcher.
     XrResult XRAPI_CALL xrGetInstanceProcAddr(XrInstance instance, const char* name, PFN_xrVoidFunction* function) {
+        TraceLocalActivity(local);
+        TraceLoggingWriteStart(local, "xrGetInstanceProcAddr");
+
+        XrResult result;
         try {
-            return RUNTIME_NAMESPACE::GetInstance()->xrGetInstanceProcAddr(instance, name, function);
-        } catch (std::runtime_error exc) {
+            result = RUNTIME_NAMESPACE::GetInstance()->xrGetInstanceProcAddr(instance, name, function);
+        } catch (std::exception& exc) {
+            TraceLoggingWriteTagged(local, "xrGetInstanceProcAddr_Error", TLArg(exc.what(), "Error"));
             ErrorLog("xrGetInstanceProcAddr: %s\n", exc.what());
-            return XR_ERROR_RUNTIME_FAILURE;
+            result = XR_ERROR_RUNTIME_FAILURE;
         }
+
+        TraceLoggingWriteStop(local, "xrGetInstanceProcAddr", TLArg(xr::ToCString(result), "Result"));
+        if (XR_FAILED(result) && result != XR_ERROR_FUNCTION_UNSUPPORTED) {
+            ErrorLog("xrGetInstanceProcAddr failed with %s\n", xr::ToCString(result));
+        }
+
+        return result;
     }
 
 } // namespace RUNTIME_NAMESPACE
