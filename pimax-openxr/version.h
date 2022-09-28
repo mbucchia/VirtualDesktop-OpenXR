cd147ae315ae5f621c8ffdca2b10195f45a48552
Matthieu Bucchianeri <mbucchia@gmail.com>
2022-09-27 23:00:06 -0700

    Set version 0.2.7.

--- pimax-openxr/version.h
+++ pimax-openxr/version.h
@@ -1,3 +1,3 @@
 const unsigned int RuntimeVersionMajor = 0;
-const unsigned int RuntimeVersionMinor = 0;
-const unsigned int RuntimeVersionPatch = 1;
+const unsigned int RuntimeVersionMinor = 2;
+const unsigned int RuntimeVersionPatch = 7;
