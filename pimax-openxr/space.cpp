mbucchia/VirtualDesktop-OpenXR

commit 87bd49b269ccd09497ec311c9aa0aab9edb737ff

Add support for XR_VARJO_quad_views and XR_VARJO_foveated_rendering.

pimax-openxr/space.cpp
@@ -37,8 +37,13 @@ namespace pimax_openxr {
                                                        uint32_t spaceCapacityInput,
                                                        uint32_t* spaceCountOutput,
                                                        XrReferenceSpaceType* spaces) {
-        static const XrReferenceSpaceType referenceSpaces[] = {
-            XR_REFERENCE_SPACE_TYPE_VIEW, XR_REFERENCE_SPACE_TYPE_LOCAL, XR_REFERENCE_SPACE_TYPE_STAGE};
+        std::vector<XrReferenceSpaceType> referenceSpaces;
+        referenceSpaces.push_back(XR_REFERENCE_SPACE_TYPE_VIEW);
+        referenceSpaces.push_back(XR_REFERENCE_SPACE_TYPE_LOCAL);
+        referenceSpaces.push_back(XR_REFERENCE_SPACE_TYPE_STAGE);
+        if (has_XR_VARJO_foveated_rendering) {
+            referenceSpaces.push_back(XR_REFERENCE_SPACE_TYPE_COMBINED_EYE_VARJO);
+        }
 
         TraceLoggingWrite(g_traceProvider,
                           "xrEnumerateReferenceSpaces",
@@ -49,11 +54,11 @@ namespace pimax_openxr {
             return XR_ERROR_HANDLE_INVALID;
         }
 
-        if (spaceCapacityInput && spaceCapacityInput < ARRAYSIZE(referenceSpaces)) {
+        if (spaceCapacityInput && spaceCapacityInput < referenceSpaces.size()) {
             return XR_ERROR_SIZE_INSUFFICIENT;
         }
 
-        *spaceCountOutput = ARRAYSIZE(referenceSpaces);
+        *spaceCountOutput = (uint32_t)referenceSpaces.size();
         TraceLoggingWrite(g_traceProvider, "xrEnumerateReferenceSpaces", TLArg(*spaceCountOutput, "SpaceCountOutput"));
 
         if (spaceCapacityInput && spaces) {
@@ -87,7 +92,9 @@ namespace pimax_openxr {
 
         if (createInfo->referenceSpaceType != XR_REFERENCE_SPACE_TYPE_VIEW &&
             createInfo->referenceSpaceType != XR_REFERENCE_SPACE_TYPE_LOCAL &&
-            createInfo->referenceSpaceType != XR_REFERENCE_SPACE_TYPE_STAGE) {
+            createInfo->referenceSpaceType != XR_REFERENCE_SPACE_TYPE_STAGE &&
+            (!has_XR_VARJO_foveated_rendering ||
+             createInfo->referenceSpaceType != XR_REFERENCE_SPACE_TYPE_COMBINED_EYE_VARJO)) {
             return XR_ERROR_REFERENCE_SPACE_UNSUPPORTED;
         }
 
@@ -321,18 +328,44 @@ namespace pimax_openxr {
             return XR_ERROR_HANDLE_INVALID;
         }
 
-        if (viewLocateInfo->viewConfigurationType != XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO) {
+        if (viewLocateInfo->viewConfigurationType != XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO &&
+            (m_primaryViewConfigurationType != XR_VIEW_CONFIGURATION_TYPE_PRIMARY_QUAD_VARJO ||
+             viewLocateInfo->viewConfigurationType != XR_VIEW_CONFIGURATION_TYPE_PRIMARY_QUAD_VARJO)) {
             return XR_ERROR_VIEW_CONFIGURATION_TYPE_UNSUPPORTED;
         }
 
-        if (viewCapacityInput && viewCapacityInput < xr::StereoView::Count) {
+        if (viewCapacityInput < (viewLocateInfo->viewConfigurationType == XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO
+                                     ? xr::StereoView::Count
+                                     : xr::QuadView::Count)) {
             return XR_ERROR_SIZE_INSUFFICIENT;
         }
 
-        *viewCountOutput = xr::StereoView::Count;
+        *viewCountOutput =
+            (viewLocateInfo->viewConfigurationType == XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO ? xr::StereoView::Count
+                                                                                                : xr::QuadView::Count);
         TraceLoggingWrite(g_traceProvider, "xrLocateViews", TLArg(*viewCountOutput, "ViewCountOutput"));
 
         if (viewCapacityInput && views) {
+            // Override default to specify whether foveated rendering is desired when the application does not specify.
+            bool foveatedRenderingActive =
+                viewLocateInfo->viewConfigurationType == XR_VIEW_CONFIGURATION_TYPE_PRIMARY_QUAD_VARJO &&
+                m_preferFoveatedRendering;
+
+            if (has_XR_VARJO_foveated_rendering) {
+                const XrViewLocateFoveatedRenderingVARJO* foveatedLocate =
+                    reinterpret_cast<const XrViewLocateFoveatedRenderingVARJO*>(viewLocateInfo->next);
+                while (foveatedLocate) {
+                    if (foveatedLocate->type == XR_TYPE_VIEW_LOCATE_FOVEATED_RENDERING_VARJO) {
+                        foveatedRenderingActive = foveatedLocate->foveatedRenderingActive;
+                        break;
+                    }
+                    foveatedLocate = reinterpret_cast<const XrViewLocateFoveatedRenderingVARJO*>(foveatedLocate->next);
+                }
+
+                TraceLoggingWrite(
+                    g_traceProvider, "xrLocateViews", TLArg(foveatedRenderingActive, "FoveatedRenderingActive"));
+            }
+
             // Get the HMD pose in the base space.
             XrSpaceLocation location{XR_TYPE_SPACE_LOCATION};
             const auto result =
@@ -341,6 +374,15 @@ namespace pimax_openxr {
                 return result;
             }
             viewState->viewStateFlags = location.locationFlags;
+
+            // Query the eye tracker if needed.
+            bool isGazeValid = false;
+            XrVector3f gazeUnitVector{};
+            if (foveatedRenderingActive) {
+                double dummyTime;
+                isGazeValid =
+                    getEyeGaze(viewLocateInfo->displayTime, false /* getStateOnly */, gazeUnitVector, dummyTime);
+            }
 
             if (viewState->viewStateFlags & (XR_VIEW_STATE_POSITION_VALID_BIT | XR_VIEW_STATE_ORIENTATION_VALID_BIT)) {
                 // Calculate poses for each eye.
@@ -351,18 +393,41 @@ namespace pimax_openxr {
                 pvrPosef eyePoses[xr::StereoView::Count]{{}, {}};
                 pvr_calcEyePoses(m_pvr, xrPoseToPvrPose(location.pose), hmdToEyePose, eyePoses);
 
+                TraceLoggingWrite(g_traceProvider, "xrLocateViews", TLArg(viewState->viewStateFlags, "ViewStateFlags"));
+
                 for (uint32_t i = 0; i < *viewCountOutput; i++) {
                     if (views[i].type != XR_TYPE_VIEW) {
                         return XR_ERROR_VALIDATION_FAILURE;
                     }
 
-                    views[i].pose = pvrPoseToXrPose(eyePoses[i]);
-                    views[i].fov = m_cachedEyeFov[i];
+                    XrView viewForGazeProjection{};
+                    if (i >= xr::StereoView::Count && isGazeValid) {
+                        viewForGazeProjection.pose = pvrPoseToXrPose(hmdToEyePose[i - 2]);
+                        viewForGazeProjection.fov = views[i - 2].fov;
+                    }
+                    views[i].pose = pvrPoseToXrPose(eyePoses[i % xr::StereoView::Count]);
+                    XrVector2f projectedGaze;
+                    if (i < xr::StereoView::Count || !isGazeValid ||
+                        !ProjectPoint(viewForGazeProjection, gazeUnitVector, projectedGaze)) {
+                        views[i].fov = m_cachedEyeFov[i];
 
-                    TraceLoggingWrite(
-                        g_traceProvider, "xrLocateViews", TLArg(viewState->viewStateFlags, "ViewStateFlags"));
+                    } else {
+                        // Shift FOV according to the eye gaze.
+                        // TODO: Need to widen the FOV when near the edges of the headset.
+                        XrFovf globalFov = m_cachedEyeFov[i % xr::StereoView::Count];
+                        std::tie(views[i].fov.angleLeft, views[i].fov.angleRight) =
+                            Fov::Lerp(std::make_pair(globalFov.angleLeft, globalFov.angleRight),
+                                      std::make_pair(m_cachedEyeFov[i + 2].angleLeft, m_cachedEyeFov[i + 2].angleRight),
+                                      (projectedGaze.x + 1.f) / 2.f);
+                        std::tie(views[i].fov.angleDown, views[i].fov.angleUp) =
+                            Fov::Lerp(std::make_pair(globalFov.angleDown, globalFov.angleUp),
+                                      std::make_pair(m_cachedEyeFov[i + 2].angleDown, m_cachedEyeFov[i + 2].angleUp),
+                                      (1.f - projectedGaze.y) / 2.f);
+                    }
+
                     TraceLoggingWrite(g_traceProvider,
                                       "xrLocateViews",
+                                      TLArg(i, "ViewIndex"),
                                       TLArg(xr::ToString(views[i].pose).c_str(), "Pose"),
                                       TLArg(xr::ToString(views[i].fov).c_str(), "Fov"));
                 }
@@ -422,6 +487,14 @@ namespace pimax_openxr {
                       XR_SPACE_LOCATION_POSITION_VALID_BIT | XR_SPACE_LOCATION_POSITION_TRACKED_BIT);
             if (velocity) {
                 velocity->velocityFlags = XR_SPACE_VELOCITY_ANGULAR_VALID_BIT | XR_SPACE_VELOCITY_LINEAR_VALID_BIT;
+            }
+        } else if (xrSpace.referenceType == XR_REFERENCE_SPACE_TYPE_COMBINED_EYE_VARJO) {
+            pose = Pose::Identity();
+
+            XrVector3f dummyVector{};
+            double dummyTime;
+            if (getEyeGaze(time, true /* getStateOnly */, dummyVector, dummyTime)) {
+                result = XR_SPACE_LOCATION_ORIENTATION_TRACKED_BIT;
             }
         } else if (xrSpace.action != XR_NULL_HANDLE) {
             // Action spaces for motion controllers.

companion/ExperimentalSettings.cs
@@ -85,6 +85,14 @@ namespace companion
                 alwaysUseFrameIdZero.Checked = (int)key.GetValue("quirk_always_use_frame_id_zero", 0) == 1 ? true : false;
                 forceDisableParallelProjection.Checked = (int)key.GetValue("force_parallel_projection_state", 1) == 0 ? true : false;
                 droolonProjectionDistance.Value = (int)key.GetValue("droolon_projection_distance", 35);
+                enableQuadViews.Checked = (int)key.GetValue("disable_quad_views", 1) == 0 ? true : false;
+                focusDensity.Value = (int)key.GetValue("focus_density", 1000);
+                peripheralDensity.Value = (int)key.GetValue("peripheral_density", 500);
+                horizontalSection1.Value = (int)key.GetValue("focus_horizontal_section", 750);
+                horizontalSection2.Value = (int)key.GetValue("focus_horizontal_section_foveated", 500);
+                verticalSection1.Value = (int)key.GetValue("focus_vertical_section", 700);
+                verticalSection2.Value = (int)key.GetValue("focus_vertical_section_foveated", 500);
+                preferFoveated.Checked = (int)key.GetValue("prefer_foveated_rendering", 1) == 1 ? true : false;
 
                 // DO NOT FORGET TO ADD TO restoreDefaults_Click()!
             }
@@ -104,9 +112,14 @@ namespace companion
             filterLength_Scroll(null, null);
             timingBias_Scroll(null, null);
             droolonProjectionDistance_Scroll(null, null);
+            focusDensity_Scroll(null, null);
+            peripheralDensity_Scroll(null, null);
+            horizontalSection1_Scroll(null, null);
+            horizontalSection2_Scroll(null, null);
+            verticalSection1_Scroll(null, null);
+            verticalSection2_Scroll(null, null);
 
             ResumeLayout();
-
             loading = false;
         }
 
@@ -115,6 +128,13 @@ namespace companion
             forceRateLabel.Enabled = forceHalf.Enabled = forceThird.Enabled = enableFrameTiming.Checked;
             filterLength.Enabled = filterLengthLabel.Enabled = filterLengthValue.Enabled =
                 timingBias.Enabled = timingBiasLabel.Enabled = timingBiasValue.Enabled = enableFrameTiming.Checked && !(forceHalf.Checked || forceThird.Checked);
+            focusDensity.Enabled = focusDensityLabel.Enabled = focusDensityValue.Enabled =
+                peripheralDensity.Enabled = peripheralDensityLabel.Enabled = peripheralDensityValue.Enabled =
+                horizontalSection1.Enabled = horizontalSection1Label.Enabled = horizontalSection1Value.Enabled =
+                horizontalSection2.Enabled = horizontalSection2Label.Enabled = horizontalSection2Value.Enabled =
+                verticalSection1.Enabled = verticalSection1Label.Enabled = verticalSection1Value.Enabled =
+                verticalSection2.Enabled = verticalSection2Label.Enabled = verticalSection2Value.Enabled =
+                preferFoveated.Enabled = enableQuadViews.Checked;
         }
 
         private void enableFrameTiming_CheckedChanged(object sender, EventArgs e)
@@ -264,6 +284,112 @@ namespace companion
             MainForm.WriteSetting("droolon_projection_distance", droolonProjectionDistance.Value);
         }
 
+        private void enableQuadViews_CheckedChanged(object sender, EventArgs e)
+        {
+            RefreshEnabledState();
+
+            if (loading)
+            {
+                return;
+            }
+
+            MainForm.WriteSetting("disable_quad_views", enableQuadViews.Checked ? 0 : 1);
+        }
+
+        private void focusDensity_Scroll(object sender, EventArgs e)
+        {
+            // Use the input in tenth of percent to allow one decimal.
+            focusDensityValue.Text = (focusDensity.Value / 10.0f).ToString("#.#");
+
+            if (loading)
+            {
+                return;
+            }
+
+            // Store in tenth of percent.
+            MainForm.WriteSetting("focus_density", focusDensity.Value);
+        }
+
+        private void peripheralDensity_Scroll(object sender, EventArgs e)
+        {
+            // Use the input in tenth of percent to allow one decimal.
+            peripheralDensityValue.Text = (peripheralDensity.Value / 10.0f).ToString("#.#");
+
+            if (loading)
+            {
+                return;
+            }
+
+            // Store in tenth of percent.
+            MainForm.WriteSetting("peripheral_density", peripheralDensity.Value);
+        }
+
+        private void horizontalSection1_Scroll(object sender, EventArgs e)
+        {
+            // Use the input in tenth of percent to allow one decimal.
+            horizontalSection1Value.Text = (horizontalSection1.Value / 10.0f).ToString("#.#");
+
+            if (loading)
+            {
+                return;
+            }
+
+            // Store in tenth of percent.
+            MainForm.WriteSetting("focus_horizontal_section", horizontalSection1.Value);
+        }
+
+        private void horizontalSection2_Scroll(object sender, EventArgs e)
+        {
+            // Use the input in tenth of percent to allow one decimal.
+            horizontalSection2Value.Text = (horizontalSection2.Value / 10.0f).ToString("#.#");
+
+            if (loading)
+            {
+                return;
+            }
+
+            // Store in tenth of percent.
+            MainForm.WriteSetting("focus_horizontal_section_foveated", horizontalSection2.Value);
+        }
+
+        private void verticalSection1_Scroll(object sender, EventArgs e)
+        {
+            // Use the input in tenth of percent to allow one decimal.
+            verticalSection1Value.Text = (verticalSection1.Value / 10.0f).ToString("#.#");
+
+            if (loading)
+            {
+                return;
+            }
+
+            // Store in tenth of percent.
+            MainForm.WriteSetting("focus_vertical_section", verticalSection1.Value);
+        }
+
+        private void verticalSection2_Scroll(object sender, EventArgs e)
+        {
+            // Use the input in tenth of percent to allow one decimal.
+            verticalSection2Value.Text = (verticalSection2.Value / 10.0f).ToString("#.#");
+
+            if (loading)
+            {
+                return;
+            }
+
+            // Store in tenth of percent.
+            MainForm.WriteSetting("focus_vertical_section_foveated", verticalSection2.Value);
+        }
+
+        private void preferFoveated_CheckedChanged(object sender, EventArgs e)
+        {
+            if (loading)
+            {
+                return;
+            }
+
+            MainForm.WriteSetting("prefer_foveated_rendering", preferFoveated.Checked ? 1 : 0);
+        }
+
         private void restoreDefaults_Click(object sender, EventArgs e)
         {
             Microsoft.Win32.RegistryKey key = null;
@@ -280,6 +406,14 @@ namespace companion
                 key.DeleteValue("quirk_always_use_frame_id_zero", false);
                 key.DeleteValue("force_parallel_projection_state", false);
                 key.DeleteValue("droolon_projection_distance", false);
+                key.DeleteValue("disable_quad_views", false);
+                key.DeleteValue("focus_density", false);
+                key.DeleteValue("peripheral_density", false);
+                key.DeleteValue("focus_horizontal_section", false);
+                key.DeleteValue("focus_horizontal_section_foveated", false);
+                key.DeleteValue("focus_vertical_section", false);
+                key.DeleteValue("focus_vertical_section_foveated", false);
+                key.DeleteValue("prefer_foveated_rendering", false);
             }
             catch (Exception)
             {

pimax-openxr/framework/dispatch.gen.cpp
@@ -1824,6 +1824,12 @@ namespace RUNTIME_NAMESPACE {
 		else if (extensionName == "XR_EXT_eye_gaze_interaction") {
 			has_XR_EXT_eye_gaze_interaction = true;
 		}
+		else if (extensionName == "XR_VARJO_quad_views") {
+			has_XR_VARJO_quad_views = true;
+		}
+		else if (extensionName == "XR_VARJO_foveated_rendering") {
+			has_XR_VARJO_foveated_rendering = true;
+		}
 
 	}
 

pimax-openxr/framework/dispatch.gen.h
@@ -133,6 +133,8 @@ namespace RUNTIME_NAMESPACE {
 		bool has_XR_EXT_hand_tracking{false};
 		bool has_XR_EXT_hand_joints_motion_range{false};
 		bool has_XR_EXT_eye_gaze_interaction{false};
+		bool has_XR_VARJO_quad_views{false};
+		bool has_XR_VARJO_foveated_rendering{false};
 
 
 	};

pimax-openxr/framework/dispatch_generator.py
@@ -40,7 +40,7 @@ from xrconventions import OpenXRConventions
 EXCLUDED_API = ['xrGetInstanceProcAddr', 'xrEnumerateApiLayerProperties']
 EXTENSIONS = ['XR_KHR_D3D11_enable', 'XR_KHR_D3D12_enable', 'XR_KHR_vulkan_enable', 'XR_KHR_vulkan_enable2', 'XR_KHR_opengl_enable',
               'XR_KHR_composition_layer_depth', 'XR_KHR_visibility_mask', 'XR_KHR_win32_convert_performance_counter_time', 'XR_FB_display_refresh_rate',
-              'XR_EXT_hand_tracking', 'XR_EXT_hand_joints_motion_range', 'XR_EXT_eye_gaze_interaction']
+              'XR_EXT_hand_tracking', 'XR_EXT_hand_joints_motion_range', 'XR_EXT_eye_gaze_interaction', 'XR_VARJO_quad_views', 'XR_VARJO_foveated_rendering']
 
 class DispatchGenOutputGenerator(AutomaticSourceOutputGenerator):
     '''Common generator utilities and formatting.'''

pimax-openxr/instance.cpp
@@ -593,6 +593,13 @@ namespace pimax_openxr {
         m_extensionsTable.push_back( // Eye tracking.
             {XR_EXT_EYE_GAZE_INTERACTION_EXTENSION_NAME, XR_EXT_eye_gaze_interaction_SPEC_VERSION});
 
+        if (!getSetting("disable_quad_views").value_or(1)) {
+            m_extensionsTable.push_back( // Quad views.
+                {XR_VARJO_QUAD_VIEWS_EXTENSION_NAME, XR_VARJO_quad_views_SPEC_VERSION});
+            m_extensionsTable.push_back( // Foveated rendering with quad views.
+                {XR_VARJO_FOVEATED_RENDERING_EXTENSION_NAME, XR_VARJO_foveated_rendering_SPEC_VERSION});
+        }
+
         // FIXME: Add new extensions here.
     }
 

commit b6ccff99a6023da63b1d9f200e1055c7ca5bb3d7

Fix two-call idiom bug introduced in 87bd49b.

pimax-openxr/space.cpp
@@ -334,7 +334,8 @@ namespace pimax_openxr {
             return XR_ERROR_VIEW_CONFIGURATION_TYPE_UNSUPPORTED;
         }
 
-        if (viewCapacityInput < (viewLocateInfo->viewConfigurationType == XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO
+        if (viewCapacityInput &&
+            viewCapacityInput < (viewLocateInfo->viewConfigurationType == XR_VIEW_CONFIGURATION_TYPE_PRIMARY_STEREO
                                      ? xr::StereoView::Count
                                      : xr::QuadView::Count)) {
             return XR_ERROR_SIZE_INSUFFICIENT;