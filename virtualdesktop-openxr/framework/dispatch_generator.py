10505adde31501490e3785420e2be3c69cbb3dc6
Matthieu Bucchianeri <mbucchia@gmail.com>
2024-01-20 16:17:06 -0800

    Update Copyright notice.

--- virtualdesktop-openxr/framework/dispatch_generator.py
+++ virtualdesktop-openxr/framework/dispatch_generator.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright(c) 2022-2023 Matthieu Bucchianeri
+# Copyright(c) 2022-2024 Matthieu Bucchianeri
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this softwareand associated documentation files(the "Software"), to deal
@@ -63,7 +63,7 @@ class DispatchGenOutputGenerator(AutomaticSourceOutputGenerator):
     def outputCopywriteHeader(self):
         copyright = '''// MIT License
 //
-// Copyright(c) 2022-2023 Matthieu Bucchianeri
+// Copyright(c) 2022-2024 Matthieu Bucchianeri
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this softwareand associated documentation files(the "Software"), to deal
