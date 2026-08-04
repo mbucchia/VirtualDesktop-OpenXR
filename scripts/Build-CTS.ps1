f6b49941318e0e09007c34fe0ba40abc85cba435
Matthieu Bucchianeri <mbucchia@gmail.com>
2026-08-04 14:23:43 -0700

    Update OpenXR SDK and CTS to latest.

--- scripts/Build-CTS.ps1
+++ scripts/Build-CTS.ps1
@@ -17,22 +17,4 @@ if (-Not $?)
     throw "CMake build failed: $LastExitCode"
 }
 
-# Cleanup past prebuilt.
-If (Test-Path $PSScriptRoot\..\bin\x64\CTS\output)
-{
-    Remove-Item $PSScriptRoot\..\bin\x64\CTS\output -Recurse -Force
-}
-
-New-Item $PSScriptRoot\..\bin\x64\CTS\output -ItemType Directory | Out-Null
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_cli\$BuildType\conformance_cli.exe $PSScriptRoot\..\bin\x64\CTS\output -Force
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_cli\$BuildType\conformance_cli.pdb $PSScriptRoot\..\bin\x64\CTS\output -Force
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_test\$BuildType\conformance_test.dll $PSScriptRoot\..\bin\x64\CTS\output -Force
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_test\$BuildType\conformance_test.pdb $PSScriptRoot\..\bin\x64\CTS\output -Force
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_cli\*.png $PSScriptRoot\..\bin\x64\CTS\output -Force
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_cli\*.otf $PSScriptRoot\..\bin\x64\CTS\output -Force
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_test\$BuildType\openxr_loader.dll $PSScriptRoot\..\bin\x64\CTS\output -Force
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_layer\$BuildType\XrApiLayer_runtime_conformance.dll $PSScriptRoot\..\bin\x64\CTS\output -Force
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_layer\$BuildType\XrApiLayer_runtime_conformance.pdb $PSScriptRoot\..\bin\x64\CTS\output -Force
-Copy-Item $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_layer\XrApiLayer_runtime_conformance.json $PSScriptRoot\..\bin\x64\CTS\output -Force
-
-Write-Host "All output in: $PSScriptRoot\..\bin\x64\CTS\output"
+Write-Host "All output in: $PSScriptRoot\..\bin\x64\CTS\src\conformance\conformance_cli\$BuildType"
