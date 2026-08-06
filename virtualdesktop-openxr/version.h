da9bd9849860fe127620313af724f379ff730a0a
Matthieu Bucchianeri <mbucchia@gmail.com>
2026-08-06 09:59:33 -0700

    Prepare for release 1.1.0.

--- virtualdesktop-openxr/version.h
+++ virtualdesktop-openxr/version.h
@@ -1,3 +1,3 @@
 const unsigned int RuntimeVersionMajor = 1;
-const unsigned int RuntimeVersionMinor = 0;
-const unsigned int RuntimeVersionPatch = 10;
+const unsigned int RuntimeVersionMinor = 1;
+const unsigned int RuntimeVersionPatch = 0;
